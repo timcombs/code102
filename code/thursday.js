4a27593524ef2cacdf362a1439f8282125a6cba4
Beth Adele Long <beth@virtualcreatives.com>
2016-10-30 16:30:57 -0700

    Add Thursday code, add links to code

---
+++ code/thursday.js
@@ -0,0 +1,22 @@
+// In today's assignment, you'll "refactor" the code from yesterday.
+// After each TODO, once you get the code working, stop and add/commit
+// your changes to your repository.
+
+// TODO: Copy in your completed code from Wednesday.
+
+// TODO: Refactor all the code into a single big function
+//       called dayApp().
+
+// TODO: What happens if you reload the page now?
+
+// TODO: Add one line of code that calls your new function.
+//       Now what happens when you reload the page?
+
+// TODO: Refactor the math that gets a random day number
+//       into a function called getDayNumber().
+
+// TODO: Refactor the if/elseif/else block that comments
+//       on each day into this function:
+function remarkOnDay(day) {
+
+}
