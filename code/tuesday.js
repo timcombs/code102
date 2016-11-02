3cfaa0da4d60f4bf48da09c2bd27fd358f6d3ecb
Beth Adele Long <beth@virtualcreatives.com>
2016-11-01 20:24:51 -0700

    Add notes

--- code/tuesday.js
+++ code/tuesday.js
@@ -1,14 +1,17 @@
 var day = 'Monday';
-console.log('the day is: ' + day);
+// console.log('the day is: ' + day);
 
-day = 'Tuesday';
-console.log('the day is: ' + day);
+day == 'Monday'
+day === 'Monday'
 
-day = 'Thursday';
-console.log('the day is: ' + day);
-
-day = 1;
-console.log('the day is: ' + day);
+// day = 'Tuesday';
+// console.log('the day is: ' + day);
+//
+// day = 'Thursday';
+// console.log('the day is: ' + day);
+//
+// day = 1;
+// console.log('the day is: ' + day);
 
 var monday = 'monday';
 var tuesday = 'tuesday';
@@ -25,13 +28,13 @@ tuesday = wednesday;
 
 var number = 1;
 var numberString = '1';
-// console.log('the variable number contains: ' + number);
-// console.log('the variable numberString contains: ' + numberString);
+console.log('the variable number contains: ' + number);
+console.log('the variable numberString contains: ' + numberString);
 
 // TODO: What if we look at these in the console?
 // In the console, type:
 // number
 // numberString
 
-// console.log('the type of variable number is: ' + typeof number);
-// console.log('the type of variable numberString is: ' + typeof numberString);
+console.log('the type of variable number is: ' + typeof number);
+console.log('the type of variable numberString is: ' + typeof numberString);
---
+++ notes/tuesday.md
@@ -0,0 +1,19 @@
+. period / dot
+, comma
+! not / exclamation point / bang
+* star / asterisk / splat
+[] brackets / square brackets
+{} braces / curly braces / curly brackets
+| pipe
+<> gt & lt (greater than & less than) / pointy brackets
+/ forward slash
+\ backslash
+# pound / hash / octothorpe
+^ caret
+~ tilde
+` backtick / backquote
+% modulo
+& ampersand
+() parentheses / parens
+- hyphen
+_ underscore
